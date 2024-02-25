93974d425694180cb7ba043d0f6fd13e82042b21
Javi Corbín <jotacor@users.noreply.github.com>
2024-02-25 13:03:00 +0100

    Refactor in functions TODO: Refactor in Classes

--- app/app.py
+++ app/app.py
@@ -7,28 +7,25 @@ import logging as log
 import sys
 import requests
 
-
-def app():
-    c = Config()
-    log.basicConfig(format='[%(asctime)s][%(levelname)s] %(message)s', datefmt='%Y-%m-%dT%H:%M:%S', level=c.log_level, stream=sys.stdout)
-    log.info("Initiating...")
-
-    radarrconfig = radarr.Configuration(host=c.radarrHost)
-    radarrconfig.api_key['X-Api-Key'] = c.radarrAPIkey
-    rdr_movie = radarr.MovieApi(radarr.ApiClient(radarrconfig))
-    rdr_history = dict()
-    headers = {'X-Api-Key': c.radarrAPIkey, 'accept': 'application/json'}
+def radarr_api(config):
+    radarrconfig = radarr.Configuration(host=config.radarrHost)
+    radarrconfig.api_key['X-Api-Key'] = config.radarrAPIkey
+    rdr_movie_api = radarr.MovieApi(radarr.ApiClient(radarrconfig))
+    rdr_deleted = dict()
+    headers = {'X-Api-Key': config.radarrAPIkey, 'accept': 'application/json'}
     eventMovieFileDeleted = 6
-    response = requests.get(c.radarrHost+f'/api/v3/history?page=1&pageSize=10&eventType={eventMovieFileDeleted}', headers=headers)
+    response = requests.get(config.radarrHost+f'/api/v3/history?page=1&pageSize=10&eventType={eventMovieFileDeleted}', headers=headers)
     if response.status_code == 200:
         r = response.json()
         for deleted in r['records']:
-            rdr_history.update({deleted['movieId']: deleted['sourceTitle']})
+            rdr_deleted.update({deleted['movieId']: deleted['sourceTitle']})
     
-    ds = DownloadStation(c.dsIp, c.dsPort, c.dsUser, c.dsPassword, debug=True)
-    tlist = ds.tasks_list()
+    return rdr_movie_api, rdr_deleted
+
+def download_station(ds):
+    all_tasks = ds.tasks_list()
     ds_tasks = dict()
-    for task in tlist['data']['tasks']:
+    for task in all_tasks['data']['tasks']:
         if task['status'] == 'error' or ('tracker' in task['additional'] and not any([status for status in task['additional']['tracker'] if status['status'] == 'Success'])):
             log.info(f"Deleting '{task['title']}' not yet in tracker or '{task['status']}' from DownloadStation")
             ds.delete_task(task['id'])
@@ -36,10 +33,14 @@ def app():
 
         ds_tasks.update({task['title']: task['id']})
 
-    movies = rdr_movie.list_movie()
+    return ds_tasks
+
+def clean_deleted_movies(ds, rdr_movie_api, rdr_deleted):
+    ds_tasks = download_station(ds)
+    movies = rdr_movie_api.list_movie()
     for movie in movies:
         if not movie['has_file'] and not movie['monitored']:
-            filename = rdr_history[movie['id']].split("/")[-1]
+            filename = rdr_deleted[movie['id']].split("/")[-1]
             if filename in ds_tasks:
                 log.info(f"Deleting '{movie['title']}' from DownloadStation")
                 ds.delete_task(ds_tasks[filename])
@@ -47,7 +48,17 @@ def app():
                 log.warning(f"Movie '{movie['title']}' not found in DownloadStation")
             
             log.info(f"Deleting '{movie['title']}' movie from Radarr")
-            rdr_movie.delete_movie(movie['id'])
+            rdr_movie_api.delete_movie(movie['id'])
+
+def app():
+    config = Config()
+    log.basicConfig(format='[%(asctime)s][%(levelname)s] %(message)s', datefmt='%Y-%m-%dT%H:%M:%S', level=config.log_level, stream=sys.stdout)
+    log.info("Initiating...")
+
+    rdr_movie_api, rdr_deleted = radarr_api(config)
+    ds = DownloadStation(config.dsIp, config.dsPort, config.dsUser, config.dsPassword, debug=True)
+
+    clean_deleted_movies(ds, rdr_movie_api, rdr_deleted)
 
     log.info("Ending...")
 
