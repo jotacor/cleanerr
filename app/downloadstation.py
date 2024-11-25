d6618f0783054899e7e86a5165c01b035bf45995
Javi Corbín <jotacor@users.noreply.github.com>
2024-11-25 10:18:38 +0100

    Fix deleting DS tasks when there is no internet connection

--- app/downloadstation.py
+++ app/downloadstation.py
@@ -25,7 +25,7 @@ class DownloadStation:
     def delete_no_tracked(self):
         all_tasks = self.ds.tasks_list()
         for task in all_tasks['data']['tasks']:
-            if task['status'] == 'error' or ('tracker' in task['additional'] and not any([status for status in task['additional']['tracker'] if status['status'] == 'Success'])):
+            if task['status'] == 'error' or ('tracker' in task['additional'] and not any([status for status in task['additional']['tracker'] if status['status'] in ['Success', '', 'Could not connect to tracker']])):
                 log.info(f"DELETED DS TRACKER: '{task['title']}'")
                 if not self.config.dryrun:
                     self.ds.delete_task(task['id'])
